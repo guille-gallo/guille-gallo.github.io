44c7ccf6c43e15b126f4ad5685d374c68231d66e
Guillermo Gallo <Guillermo.Gallo@wnco.com>
2015-07-30 10:00:58 -0500

    cosa

--- main.js
+++ main.js
@@ -44,6 +44,10 @@ function initialize() {
           document.write("desiredAccuracy: ", options.desiredAccuracy, " | ");
 
           if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
+              var map = new google.maps.Map(document.getElementById('map-canvas'), {
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                panControl: true
+              });
               var defaultBounds = new google.maps.LatLngBounds(
                 new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
               );

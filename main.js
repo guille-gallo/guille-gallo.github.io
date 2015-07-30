7d7ea7bbd850e9105ffbf1b75325dbac205f1b96
Guillermo Gallo <Guillermo.Gallo@wnco.com>
2015-07-30 12:11:06 -0500

    cosa

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function initialize() {
 
           //if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
               var mapOptions = {
-                zoom: 20,
+                zoom: 19,
                 center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
                 mapTypeControlOptions: {
                   style: google.maps.MapTypeControlStyle.DEFAULT,

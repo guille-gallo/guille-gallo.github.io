c6311e5994696e569f818c9204cfaf1477c873ac
Guillermo <guille.gallo88@gmail.com>
2015-08-13 10:56:09 -0300

    calculate distance based on select selection

--- public/main.js
+++ public/main.js
@@ -11,155 +11,31 @@ function initialize() {
     
     navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, geoprogress, options) {
 
-      var lastCheckedPosition,
-          locationEventCount = 0,
-          watchID,
+      var watchID,
           timerID;
 
       options = options || {};
 
       var checkLocation = function (position) {
-          lastCheckedPosition = position;
-          locationEventCount = locationEventCount + 1;
-          // We ignore the first event unless it's the only one received because some devices seem to send a cached
-          // location even when maxaimumAge is set to zero
-
-          //if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
-              var mapOptions = {
-                zoom: 19,
-                center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-                mapTypeControlOptions: {
-                  style: google.maps.MapTypeControlStyle.DEFAULT,
-                  mapTypeIds: [
-                    google.maps.MapTypeId.ROADMAP,
-                    google.maps.MapTypeId.TERRAIN
-                  ]
-                }
-              };
-              var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-              
-              var currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-              
-              displayDirection(currentPosition, map);
-              getDistance(currentPosition, map);
-              
-              /*clearTimeout(timerID);
-              navigator.geolocation.clearWatch(watchID);*/
-      };
-
-      $('#blocksSelection').change(function() {
-        console.log( "Handler for .change() called." );
-        console.log($('#blocksSelection').find(":selected").text());
-        var getUserBlocksQtySelection = function () {
-          var userBlocksQtySelection = $('#blocksSelection').find(":selected").text();
-        }      
-      });
-
-      var displayDirection = function (currentPosition, map) {
-        var items = ['-32.951154, -60.650885', '-32.947413, -60.654120', '-32.944960, -60.643578'];
-       
-        var directionsDisplay = new google.maps.DirectionsRenderer();
-        var directionsService = new google.maps.DirectionsService();
-        
-        var request = {
-          origin: currentPosition,
-          destination:'-32.951154, -60.650885',
-          travelMode: google.maps.TravelMode.WALKING
+        var mapOptions = {
+          zoom: 19,
+          center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+          mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DEFAULT,
+            mapTypeIds: [
+              google.maps.MapTypeId.ROADMAP,
+              google.maps.MapTypeId.TERRAIN
+            ]
+          }
         };
-        directionsService.route(request, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            directionsDisplay.setMap(map);
-          }
-        });
-      }
-
-      var getDistance = function (currentPosition, map) {
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         
-        $.get( "gomerias.json", function( data ) {
-          getCoordinates(data);
-        });
-
-        var getCoordinates = function (data) {
-          var coordinates = [];
-          for (var i = 0; i < data.length; i++) {
-            coordinates[i] = data[i].coordinates;
-          }  
-          calculateDistances(coordinates);      
-        }
-
-        var markersArray = [];
-        var destinationIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=D|FF0000|000000';
-        var originIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=O|FFFF00|000000';
-
-        function calculateDistances(coordinates) {
-          console.log(coordinates);
-          var service = new google.maps.DistanceMatrixService();
-          service.getDistanceMatrix(
-            {
-              origins: [currentPosition],
-              destinations: coordinates,
-              travelMode: google.maps.TravelMode.WALKING,
-              unitSystem: google.maps.UnitSystem.METRIC,
-              avoidHighways: false,
-              avoidTolls: false
-            }, callback);
-        }
-        function callback(response, status) {
-          if (status != google.maps.DistanceMatrixStatus.OK) {
-            alert('Error was: ' + status);
-          } else {
-            var origins = response.originAddresses;
-            var destinations = response.destinationAddresses;
-            var outputDiv = document.getElementById('outputDiv');
-            outputDiv.innerHTML = '';
-            deleteOverlays();
-            
-            console.log(getUserBlocksQtySelection());
-
-            for (var i = 0; i < origins.length; i++) {
-              var results = response.rows[i].elements;
-              //addMarker(origins[i], false);
-              for (var j = 0; j < results.length; j++) {
-                //addMarker(destinations[j], true);
-                outputDiv.innerHTML += 'DESDE: ' + origins[i] + '</br>' + ' HASTA ' + destinations[j]
-                    + '</br>' + 'DISTANCIA: ' + results[j].distance.text + '</br>' + ' TIEMPO: '
-                    + results[j].duration.text + '<br>' + "-------------------------" + "</br>";
-              }
-            }
-          }
-        }
-        function addMarker(location, isDestination) {
-          var icon;
-          if (isDestination) {
-            icon = destinationIcon;
-          } else {
-            icon = originIcon;
-          }
-          geocoder.geocode({'address': location}, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-              bounds.extend(results[0].geometry.location);
-              map.fitBounds(bounds);
-              var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                icon: icon
-              });
-              markersArray.push(marker);
-            } else {
-              alert('Geocode was not successful for the following reason: '
-                + status);
-            }
-          });
-        }
-        function deleteOverlays() {
-          for (var i = 0; i < markersArray.length; i++) {
-            markersArray[i].setMap(null);
-          }
-          markersArray = [];
-        }
+        var currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        getDistance(currentPosition, map);
       };
-
+//------------------------------------------------------------------------------------------------------------------------------------------
+//      GEOLOCATION FUNCTIONS:
+//------------------------------------------------------------------------------------------------------------------------------------------
       // aparentemente esto se ejecuta al retornar la primer ubicación o cuando ya no va a intenar más obtener una ubicación.
       var stopTrying = function () {
           navigator.geolocation.clearWatch(watchID);
@@ -191,7 +67,88 @@ function initialize() {
       watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
       timerID = setTimeout(stopTrying, options.maxWait); // Set a timeout that will abandon the location loop
     };
-    navigator.geolocation.getAccurateCurrentPosition({desiredAccuracy:20, maxWait:15000});
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+    var getDistance = function (currentPosition, map) {
+
+      $.get( "gomerias.json", function( data ) {
+        getCoordinates(data);
+      });
+
+      var getCoordinates = function (data) {
+        var coordinates = [];
+        for (var i = 0; i < data.length; i++) {
+          coordinates[i] = data[i].coordinates;
+        }  
+        calculateDistances(coordinates);      
+      }
+
+      var destinationIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=D|FF0000|000000';
+      var originIcon = 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=O|FFFF00|000000';
+
+      function calculateDistances(coordinates) {
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix(
+          {
+            origins: [currentPosition],
+            destinations: coordinates,
+            travelMode: google.maps.TravelMode.WALKING,
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+          }, callback);
+      }
+      function callback(response, status) {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+          alert('Error was: ' + status);
+        } else {
+          var origins = response.originAddresses;
+          var destinations = response.destinationAddresses;
+          var outputDiv = document.getElementById('outputDiv');
+
+          $('#blocksSelection').change(function() {
+            outputDiv.innerHTML = ''; 
+
+            for (var i = 0; i < origins.length; i++) {
+              var results = response.rows[i].elements;
+
+              for (var j = 0; j < results.length; j++) {
+                var userBlocksQtySelection = $('#blocksSelection').find(":selected").text();
+                var metersSelection = (userBlocksQtySelection * 100);
+
+                if (results[j].distance.value <= metersSelection) {
+                  outputDiv.innerHTML += 'DESDE: ' + origins[i] + '</br>' + ' HASTA: ' + destinations[j]
+                    + '</br>' + 'DISTANCIA: ' + results[j].distance.text + '</br>' + ' TIEMPO: '
+                    + results[j].duration.text + '<br>' + "-------------------------" + "</br>";
+                } else {
+                  console.log("no matches");
+                }            
+              };
+            }
+          });         
+        }
+      }
+    };
+
+    var displayDirection = function (currentPosition, map) {     
+      var directionsDisplay = new google.maps.DirectionsRenderer();
+      var directionsService = new google.maps.DirectionsService();
+      
+      var request = {
+        origin: currentPosition,
+        destination:'-32.951154, -60.650885',
+        travelMode: google.maps.TravelMode.WALKING
+      };
+
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+          directionsDisplay.setMap(map);
+        }
+      });
+    }
+
+    navigator.geolocation.getAccurateCurrentPosition({maxWait:15000});
   }
 }
 google.maps.event.addDomListener(window, 'load', initialize);

ab03b576ddded09ce60e6480a9c8ed8ceaf8c3f3
Guillermo <guille.gallo88@gmail.com>
2015-08-14 15:14:54 -0300

    first angular structure

---
+++ public/angular-seed/js/app.js
@@ -0,0 +1,20 @@
+var pincheGomaApp = angular.module('pincheGomaApp', [
+  'ngRoute'
+  //'expressTestAppControllers'
+]);
+
+/*expressTestApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      }).
+      when('/articles-list', {
+        templateUrl: 'partials/articles-list.html',
+        controller: 'ArticlesListCtrl'
+      }).
+      otherwise({
+        redirectTo: '/login'
+      });
+}]);*/
---
+++ public/angular-seed/js/controllers.js
@@ -0,0 +1,1 @@
+var expressTestAppControllers = angular.module('expressTestAppControllers', []);
